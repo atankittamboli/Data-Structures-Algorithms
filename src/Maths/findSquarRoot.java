2bfdea4a3ee917a013842794e8f16f1840df2909
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-10 23:06:09 +0530

    10-feb-2025

---
+++ src/Maths/findSquarRoot.java
@@ -0,0 +1,12 @@
+package Maths;
+
+public class findSquarRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double d = Math.sqrt(8);
+		Math.floor(d);
+		System.out.println();
+	}
+
+}
