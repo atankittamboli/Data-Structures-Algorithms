71e5c2e78ace301a527d822e7e189ee3bf230821
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-11 23:15:06 +0530

    11-feb-2025

--- src/string/LongestSubString_UniqueCharacter.java
+++ src/string/LongestSubString_UniqueCharacter.java
@@ -3,77 +3,32 @@
 package string;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LongestSubString_UniqueCharacter {
 
-	static final int MAX_CHARS = 256;
 	
 	public static void main(String[] args) {
-		String str  = "aabcbcdbca";
-						
-		String result  = longestSubString(str);
-		
-		System.out.println(result);		
+		String str  = "abcabcbb";
+								
+		System.out.println(lengthOfLongestSubstring(str));		
 	}
 
-	public static String longestSubString(String str) {
-		
-		
-		int length = str.length();
-		
-		if(length<1) {
-			return "invalid String";
-		}
-		
-		int dist_count = 0;
-		boolean[] visited = new boolean[MAX_CHARS];
-		Arrays.fill(visited, false);
-		 
-		for(int i=0; i<length; i++) {
-			if(visited[str.charAt(i)]==false) {
-				visited[str.charAt(i)]=true;
-				dist_count++;
+	public static int lengthOfLongestSubstring(String s) {
+		int left = 0, right = 0, maxLength = 0;
+		HashSet<Character> set = new HashSet<>();
+
+		while (right < s.length()) {
+			if (!set.contains(s.charAt(right))) {
+				set.add(s.charAt(right));
+				maxLength = Math.max(maxLength, right - left + 1);
+				right++; // Expand window
+			} else {
+				set.remove(s.charAt(left));
+				left++; // Shrink window
 			}
 		}
-		
-		int start = 0, start_index = -1;
-        int min_len = Integer.MAX_VALUE;
-        int count = 0;
-        
-        int[] curr_count = new int[MAX_CHARS];
-        
-        
-        for (int j = 0; j <length; j++) {
-        	curr_count[str.charAt(j)]++;
-        	
-        	if (curr_count[str.charAt(j)] == 1)
-                count++;
-        	
-        	// if all the characters are matched
-            if (count == dist_count) {
-                // Try to minimize the window i.e., check if
-                // any character is occurring more no. of
-                // times than its occurrence in pattern, if
-                // yes then remove it from starting and also
-                // remove the useless characters.
-                while (curr_count[str.charAt(start)] > 1) {
-                    if (curr_count[str.charAt(start)] > 1)
-                        curr_count[str.charAt(start)]--;
-                    start++;
-                }
 
-                // Update window size
-                int len_window = j - start + 1;
-                if (min_len > len_window) {
-                    min_len = len_window;
-                    start_index = start;
-                }
-            }
-        }
-                
-        // Return substring starting from start_index
-        // and length min_len
-        return str.substring(start_index,
-                             start_index + min_len);
+		return maxLength;
 	}
 }
