18ab59f3545deffcfa624d85cbf42346dfaf861e
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-26 23:46:45 +0530

    26-feb-2025

---
+++ src/LinkedList/IntersectionofTwoLinkedLists_160.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class IntersectionofTwoLinkedLists_160 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static Node getIntersectionNode(Node headA, Node headB) {
+        if (headA == null || headB == null) return null;
+        
+        Node a = headA, b = headB;
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+        return a;
+    }
+}
