0f2af94fe5dafa3c1626ab69e0343b7186cca201
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-27 23:49:39 +0530

    27-feb-2025

---
+++ src/LinkedList/PalindromeLinkedList_234.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class PalindromeLinkedList_234 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isPalindrome(Node head) {
+	    if (head == null || head.next == null) return true;
+
+	    Node slow = head, fast = head;
+	    while (fast != null && fast.next != null) {
+	        slow = slow.next;
+	        fast = fast.next.next;
+	    }
+
+	    // Reverse second half
+	    Node secondHalf = reverseList(slow);
+	    Node firstHalf = head;
+
+	    while (secondHalf != null) {
+	        if (firstHalf.val != secondHalf.val) return false;
+	        firstHalf = firstHalf.next;
+	        secondHalf = secondHalf.next;
+	    }
+	    return true;
+	}
+
+	private Node reverseList(Node head) {
+		Node prev = null, curr = head;
+	    while (curr != null) {
+	    	Node nextNode = curr.next;
+	        curr.next = prev;
+	        prev = curr;
+	        curr = nextNode;
+	    }
+	    return prev;
+	}
+
+}
