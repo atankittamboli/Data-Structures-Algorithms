3a9cb2294ba1492951b5bf4955f696c15758182e
Ankit Tamboli <atankittamboli@gmail.com>
2025-03-08 23:45:48 +0530

    8-March-2025

--- src/array/ContainsDuplicate.java
+++ src/array/ContainsDuplicate.java
@@ -5,8 +5,7 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		int [] nums = {1,1,1,3,3,4,3,2,4,2};
 		Boolean result  = containsDuplicate(nums);
 		System.out.println(result);
