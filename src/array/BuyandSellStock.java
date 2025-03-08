628086438c124a1a596cc41b35ed56cdecf31ee2
Ankit Tamboli <atankittamboli@gmail.com>
2025-03-08 23:45:18 +0530

    8-March-2025

--- src/array/BuyandSellStock.java
+++ src/array/BuyandSellStock.java
@@ -5,8 +5,7 @@ public class BuyandSellStock {
 	public static void main(String[] args) {
 		int [] prices = {7,1,5,3,6,4};		
 		int profit  = maxProfit(prices);
-		System.out.println(profit);
-		
+		System.out.println(profit);		
 	}
 	
 	public static int maxProfit(int[] prices) {
